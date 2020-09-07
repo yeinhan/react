38688f87544d450d5ec97961d9880bb4d9d88ac6
=yeinhan <goregore8637@naver.com>
2020-09-08 02:50:41 +0900

    State/ 프롭스 Key -<li key={data[i].id}><li>

--- src/App.js
+++ src/App.js
@@ -7,12 +7,23 @@ import './App.css';
 
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      subject:{title:'WEB', sub:'World Wide Web!'}, /*프로퍼티*/
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is HyperText...'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
 render() {
   return (
     <div className="App">
-      <Subject title="WEB" sub="world wide web!"></Subject>
+      <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
       <Subject title="REACT" sub="For UI"></Subject>
-      <TOC></TOC>
+      <TOC data={this.state.contents}></TOC>
       <Content title="HTML" desc="HTML is HyperText Markup Laguage"></Content>
     </div>
   );
