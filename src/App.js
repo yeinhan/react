ddeae9eb5c2b6789c3d525524a09b093bf86a8b2
=yeinhan <goregore8637@naver.com>
2020-09-08 23:39:49 +0900

    이벤트- state변경, bind함수, setState함수

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state={
+      mode:'welcome',
       subject:{title:'WEB', sub:'World Wide Web!'}, /*프로퍼티*/
+      welcome:{title:'welcome', desc:'Hello, React!!'},
       contents:[
         {id:1, title:'HTML', desc:'HTML is HyperText...'},
         {id:2, title:'CSS', desc:'CSS is for design'},
@@ -19,13 +21,29 @@ class App extends Component{
     }
   }
 render() {
+  var _title, _desc=null;
+  if(this.state.mode == 'welcome'){
+    _title = this.state.welcome.title;
+    _title = this.state.welcome.desc;
+  }else if(this.state.mode == 'read'){
+    _title = this.state.contents[0].title;
+    _desc = this.state.contents[0].desc;
+  }
   return (
     <div className="App">
-      <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
-      <Subject title="REACT" sub="For UI"></Subject>
-      <TOC data={this.state.contents}></TOC>
-      <Content title="HTML" desc="HTML is HyperText Markup Laguage"></Content>
-    </div>
+      <Subject title={this.state.subject.title} 
+      sub={this.state.subject.sub}
+      onChangePage={function(){
+        this.setState({mode:'welcome'})
+      }.bind(this)}
+      >
+      </Subject>
+      <TOC onChangePage={function(){
+        this.setState({mode:'read'});
+      }.bind(this)}
+       data={this.state.contents}></TOC>
+      <Content title={_title} desc={_desc}></Content>
+    </div> 
   );
 }
 }
