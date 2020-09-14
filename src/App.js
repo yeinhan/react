a3b88b7223ee69f10386e4a5c32513ee7848870b
=yeinhan <goregore8637@naver.com>
2020-09-15 00:56:40 +0900

    crud - create

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import TOC from "./components/TOC";
-import Content from "./components/Content";
+import ReadContent from "./components/ReadContent";
+import CreateContent from "./components/CreateContent";
 import Subject from "./components/Subject";
+import Control from "./components/Control";
 import './App.css';
 
 
@@ -9,8 +11,9 @@ import './App.css';
 class App extends Component{
   constructor(props){
     super(props);
+    this.max_content_id = 3;
     this.state={
-      mode:'read',
+      mode:'create',
       selected_content_id:2,
       subject:{title:'WEB', sub:'World Wide Web!'}, /*프로퍼티*/
       welcome:{title:'welcome', desc:'Hello, React!!'},
@@ -18,14 +21,16 @@ class App extends Component{
         {id:1, title:'HTML', desc:'HTML is HyperText...'},
         {id:2, title:'CSS', desc:'CSS is for design'},
         {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+
       ]
     }
   }
 render() {
-  var _title, _desc=null;
+  var _title, _desc,_article=null;
   if(this.state.mode == 'welcome'){
     _title = this.state.welcome.title;
     _title = this.state.welcome.desc;
+    _article=  <ReadContent title={_title} desc={_desc}></ReadContent>
   }else if(this.state.mode == 'read'){
     var i =0;
     while(i<this.state.contents.length){
@@ -37,8 +42,25 @@ render() {
       }
       i = i+1;
     }
-
+    _article=  <ReadContent title={_title} desc={_desc}></ReadContent>
+  }else if(this.state.mode =='create'){
+    _article = <CreateContent onSubmit={function(_title, _desc){
+      //add content to this.state.contents
+      this.max_content_id = this.max_content_id+1;
+      // this.state.contents.push(
+      //   {id:this.max_content_id, title:_title, desc:_desc}
+      // );
+      var _contents = this.state.contents.concat(
+        {id:this.max_content_id, title:_title, desc:_desc}
+      );
+      this.setState({
+        contents:_contents
+      });
+      console.log(_title,_desc);
+    }.bind(this)}></CreateContent>
   }
+
+
   return (
     <div className="App">
       <Subject title={this.state.subject.title} 
@@ -55,7 +77,12 @@ render() {
         });
       }.bind(this)}
        data={this.state.contents}></TOC>
-      <Content title={_title} desc={_desc}></Content>
+      <Control onChangeMode={function(_mode){
+        this.setState({
+          mode:_mode
+        });
+      }.bind(this)}></Control>
+     {_article}
     </div> 
   );
 }
---
+++ src/components/CreateContent.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+class CreateContent extends Component{
+    render(){
+      return (
+        <article>
+          <h2>CreateContent</h2>
+          <form action="/create_process" method="post" onSubmit={function(e){
+            e.preventDefault(); //페이지 안바뀌게
+            this.props.onSubmit(
+              e.target.title.value,
+              e.target.desc.value
+            );
+          }.bind(this)}> 
+            <p><input type="text" name="title" placeholder="title"></input></p>
+            <p><textarea name="desc" placeholder="description"></textarea></p>
+            <p><input type="submit"></input></p>
+          </form>
+        </article>
+      );
+    }
+  }
+  
+  export default CreateContent;
